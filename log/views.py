30421909310c6449bad4899b528bcb7c74248a6c
Domanova.Maria <domanova@mail.ru>
2024-05-12 16:59:51 +0500

    Init commit

---
+++ log/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse 
+from django.shortcuts import render 
+import datetime
+import logging 
+
+logger = logging.getLogger(__name__) 
+
+def welcome_page(request): 
+    logger.warning('Homepage was accessed at '+str(datetime.datetime.now())+ 'hours!') 
+    return HttpResponse("<h1>Welcome to web application development course 2023 (URFU/RTF) :)</h1>") 
